9b8c3fd59fec434f7fab098c00b017f96d0c9666
SuavePirate <alexander.dunn@outlook.com>
2017-01-22 12:30:36 -0500

    set up work

---
+++ Xamarin.Flux/Xamarin.Flux/Xamarin.Flux/Startup/IoCConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Flux.Startup
+{
+    /// <summary>
+    /// Wire up some basic IoC for dependency injection
+    /// </summary>
+    public class IoCConfig
+    {
+    }
+}
