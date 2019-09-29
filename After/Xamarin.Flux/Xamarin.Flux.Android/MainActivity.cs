add35a44906bf8a8f5fbddc03e29db19d4d9bd1d
Alex Dunn <alexander.dunn@outlook.com>
2019-09-28 22:07:23 -0400

    more upgrades

--- After/Xamarin.Flux/Xamarin.Flux.Android/MainActivity.cs
+++ After/Xamarin.Flux/Xamarin.Flux.Android/MainActivity.cs
@@ -25,7 +25,7 @@ namespace Xamarin.Flux.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
--- After/Xamarin.Flux/Xamarin.Flux/App.xaml.cs
+++ After/Xamarin.Flux/Xamarin.Flux/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Xamarin.Flux.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace Xamarin.Flux
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new TodoListPage());
         }
 
         protected override void OnStart()
